16e7df03f3fda7ff4eed151de4b4020fe9a1cef6
Prasant Adhikari <pa1038@nyu.edu>
2024-07-31 17:51:27 +0545

    add a note about ORMs and fakers

--- db/seed.sql
+++ db/seed.sql
@@ -16,3 +16,14 @@ INSERT INTO tweets(content, user_id, created_at) VALUES
   ('I am user2', 2, current_timestamp),
   ('I am user3', 3, current_timestamp),
   ('I am user4', 4, current_timestamp);
+
+-- You could use in general what is called a factory pattern
+-- to generate fake data. fakerjs is an example.
+-- When using javascript or python or other languages to
+-- generate data it might be easier to use js classes
+-- or python classes instead of SQL tables to represet data.
+-- This is what Object Relational Mapping (ORM) are for:
+-- to use programming language classes as SQL table.
+-- ORM also ease other issues like data validation
+-- Example: what if we want to validate the timestamp
+-- to not be in the future or too into the past?
