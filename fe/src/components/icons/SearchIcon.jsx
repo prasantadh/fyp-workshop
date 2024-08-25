9a91ecb58145105176071d63c021564e38531e68
Sunil Thapa <sunilthapa056@outlook.com>
2024-08-25 14:49:47 +0545

    added icons

---
+++ fe/src/components/icons/SearchIcon.jsx
@@ -0,0 +1,10 @@
+const SearchIcon = () => {
+  return (
+    <svg width="64" height="64" viewBox="0 0 65 63" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M43.32 42.32L37.868 36.868M40.8133 29.7867C40.8133 35.3243 36.3243 39.8133 30.7867 39.8133C25.2491 39.8133 20.76 35.3243 20.76 29.7867C20.76 24.2491 25.2491 19.76 30.7867 19.76C36.3243 19.76 40.8133 24.2491 40.8133 29.7867Z" stroke="#001415" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    
+  )
+}
+
+export default SearchIcon
---
+++ fe/src/components/icons/WriteIcon.jsx
@@ -0,0 +1,12 @@
+const WriteIcon = () => {
+    return (
+        <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M32 22.25H28.25C24.9363 22.25 22.25 24.9363 22.25 28.25V35.75C22.25 39.0637 24.9363 41.75 28.25 41.75H35.75C39.0637 41.75 41.75 39.0637 41.75 35.75V32M32.481 31.4534L40.896 23.0384" stroke="#001415" stroke-width="3" stroke-linecap="round"/>
+        </svg>
+        
+   
+    )
+  }
+  
+  export default WriteIcon
+  
