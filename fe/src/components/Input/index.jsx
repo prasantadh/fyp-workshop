b7e0f09de82183292143c2ad80fa8fd51879d54e
Sunil Thapa <sunilthapa056@outlook.com>
2024-08-28 11:38:12 +0545

    sign up page css

--- fe/src/components/Input/index.jsx
+++ fe/src/components/Input/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./input.css";
 
-const CustomInput = ({ hint, onChange, value, isPassword }) => {
+const CustomInput = ({ hint, onChange, value, isPassword, style }) => {
   return (
     <input
       type={isPassword ? "password": "text"}
@@ -9,6 +9,7 @@ const CustomInput = ({ hint, onChange, value, isPassword }) => {
       placeholder={hint ?? "No Hints figure it out"}
       value={value}
       className="input"
+      style = {style}
     />
   );
 };
