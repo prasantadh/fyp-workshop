16c79f0da0b9afea89e8d8141bb11e2206b7b053
su8ash-Hamal <subash.hamal.dev22@gmail.com>
2024-08-31 12:03:55 +0545

    Search page completed

--- fe/src/utils/axios.js
+++ fe/src/utils/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { decodeJWT } from "./jwt_decode";
 
 export const guestInstance = axios.create({
   baseURL: "http://127.0.0.1:5000",
@@ -10,6 +11,15 @@ export const getCookie = (name) => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()?.split(";").shift() || null;
+  return null;
+};
+
+export const getCurrentUserId = (name) => {
+  const token = getCookie("tokenFromServer");
+  if (token) {
+    let data = decodeJWT(token);
+    return data.sub;
+  }
   return null;
 };
 
